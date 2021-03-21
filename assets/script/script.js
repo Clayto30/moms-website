172ed3ee25eebec33f46e25164f76f8674a97746
Clayton Goff <cltngoff@gmail.com>
2021-03-21 10:47:40 -0700

    birthday message java

---
+++ assets/script/script.js
@@ -0,0 +1,10 @@
+        fetch(`https://api.giphy.com/v1/gifs/search?q=unicorn&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            let num2 = Math.floor(Math.random() * 50);
+            var gifImg = document.createElement('img');
+            gifImg.setAttribute('src', response.data[num2].images.fixed_height.url);
+            $("#unicorn").append(gifImg);
+        });
